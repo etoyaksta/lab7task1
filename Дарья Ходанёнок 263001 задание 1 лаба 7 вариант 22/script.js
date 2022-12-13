7eabbaeeeed553267a95a8dbcc42c2af23809289
etoyaksta <60542478+etoyaksta@users.noreply.github.com>
2022-12-13 16:41:57 +0300

    Add files via upload

---
+++ Дарья Ходанёнок 263001 задание 1 лаба 7 вариант 22/script.js
@@ -0,0 +1,11 @@
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    grid: {
+        rows: 2,
+    },
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+}); 
